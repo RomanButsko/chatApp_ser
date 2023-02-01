6edd3fc498e1ab06917a3b64063f66dcbf406372
RomanButsko <75183199+RomanButsko@users.noreply.github.com>
2023-02-01 22:49:35 +0300

    cors added all sources

--- src/messages/messages.gateway.ts
+++ src/messages/messages.gateway.ts
@@ -5,7 +5,11 @@ import { Socket } from 'socket.io';
 import { UsersService } from 'src/users/users.service';
 
 @WebSocketGateway(Number(process.env.PORT) || 8080, {
-    cors: true,
+    cors: {
+        origin: "*",
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        allowedHeaders: "Content-Type, Authorization"
+    },
     namespace: 'mail',
 })
 
