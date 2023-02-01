e9e48e263fe48eaba2358f02992e327563788446
RomanButsko <75183199+RomanButsko@users.noreply.github.com>
2023-02-01 23:33:47 +0300

    cors changed

--- src/messages/messages.gateway.ts
+++ src/messages/messages.gateway.ts
@@ -4,7 +4,7 @@ import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from
 import { Socket } from 'socket.io';
 import { UsersService } from 'src/users/users.service';
 
-@WebSocketGateway(Number(process.env.PORT) || 8080, {
+@WebSocketGateway(8080, {
      cors: {
         origin: "*",
     },
