d9a461e2ec48017383b34764d4dd0021710ecbc0
RomanButsko <75183199+RomanButsko@users.noreply.github.com>
2023-02-01 19:42:21 +0300

    changed port

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
       'Origin,X-Requested-With,Content-Type,Accept,Authorization,authorization,X-Forwarded-for',
   });
 
-  await app.listen(1018);
+  await app.listen(1024);
 }
 bootstrap();
