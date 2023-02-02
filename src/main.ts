d8ce899ab3bfda851cb51f75d0b9876a747b27cd
RomanButsko <75183199+RomanButsko@users.noreply.github.com>
2023-02-02 12:28:28 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: [/^(.*)/],
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 200,
