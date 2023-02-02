ec6d4ab763b603433d200436f08859007a996d0d
RomanButsko <75183199+RomanButsko@users.noreply.github.com>
2023-02-02 14:30:01 +0300

    fix cors

--- src/messages/messages.gateway.ts
+++ src/messages/messages.gateway.ts
@@ -4,7 +4,7 @@ import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from
 import { Socket } from 'socket.io';
 import { UsersService } from 'src/users/users.service';
 
-@WebSocketGateway({ cors: '*' })
+@WebSocketGateway({ cors: '*',  transports: ['websocket',  'polling'] })
 
 export class ChatGateway {
     constructor(private messageService: MessagesService,
